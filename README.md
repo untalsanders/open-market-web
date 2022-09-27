e3880a8f6d93d1d619e2ba2e39948042ff8d0be6
Sanders Gutiérrez <ing.sanders@gmail.com>
2022-09-27 19:00:08 -0300

    feat: First pre-version
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Static Website
+
+This is an example about how to deploy a static site on GitHub Pages with [gh-pages](https://www.npmjs.com/package/gh-pages)
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,21 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const dotenv = require('dotenv')
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+}
 
 module.exports = {
-    mode: 'development',
+    mode: process.env.NODE_ENV || 'production',
     entry: './src/app.js',
     devtool: 'inline-source-map',
     devServer: {
-        static: './public',
+        static: process.env.OUTPUT_DIR || './docs',
     },
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, process.env.OUTPUT_DIR || 'docs'),
         clean: true,
     },
     module: {
@@ -25,6 +30,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Example Static Website',
+            meta: {
+                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+            },
             template: './src/index.html',
             inject: 'body',
         }),
