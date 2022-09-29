0867bec1f09e122f731386427b98ee7713f29a48
Sanders Gutiérrez <ing.sanders@gmail.com>
2022-09-29 14:41:48 -0300

    feat: Add react to project
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
