268c125059bb7d35fd26aa1b0c839297bab84d91
Sanders Gutiérrez <ing.sanders@gmail.com>
2022-09-29 16:36:49 -0300

    fix: Update config eslint to fix error jsx-runtime
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 'use strict'
 
-import React from 'react'
 import * as ReactDOM from 'react-dom/client'
 
+const element = <h1>Welcome to My Site - Sanders</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Welcome to My Site - Sanders</h1>)
+root.render(element)
