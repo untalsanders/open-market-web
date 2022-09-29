4b0c9a532038f2f7e9437e6a9e218dff26aaead3
Sanders Gutérrez <ing.sanders@gmail.com>
2022-09-29 14:45:18 -0300

    feat: Add react to project
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
 'use strict'
+
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<h1>Welcome to My Site - Sanders</h1>)
